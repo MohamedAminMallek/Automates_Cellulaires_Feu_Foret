8eabcaf6b149f4324a2b9af7b45a77046220b319
MohamedAminMallek <med.amin.mallek@gmail.com>
2020-01-25 12:28:17 +0100

    last commit

--- simulation_stats.py
+++ simulation_stats.py
@@ -2,20 +2,21 @@ from functions import *
 
 
 
-for fire_takes_corner in [False,True]:
-    r = 0.1
-    x_plot = []
-    y_plot = []
-    while(r<=1):
-        moy = moy_simulation(r,False)
-        print("densite = " + str(r) + " :  pourcentage des arbres restants ="+ str(moy))
-        y_plot.append(moy)
-        x_plot.append(r)
-        r+=0.01
-
-    plt.plot(x_plot,y_plot)
-    plt.xlabel('Densite')
-    plt.ylabel('Arbres restants')
-    filename = ("fire_takes_corner" if fire_takes_corner else "fire_can't_takes_corner") + ".png"
-    plt.savefig(filename)
+fire_takes_corner = True
+r = 0.1
+x_plot = []
+y_plot = []
+while(r<=1):
+	moy = moy_simulation(r,fire_takes_corner)
+	print("densite = " + str(r) + " :  pourcentage des arbres restants ="+ str(moy))
+	y_plot.append(moy)
+	x_plot.append(r)
+	r+=0.01
+filename = ("fire_takes_corner" if fire_takes_corner else "fire_can't_takes_corner") + ".png"
+plt.plot(x_plot,y_plot)
+plt.xlabel('Densite')
+plt.ylabel('Arbres restants')
+plt.title = filename
+plt.filename = filename
+plt.savefig(filename)
     #plt.show()
